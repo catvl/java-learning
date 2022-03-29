6f212a00c891c331fb294ead1e1ad70e0a6edd3f
catvl <katrin030202@mail.ru>
2022-03-29 22:36:25 +0600

    impl second project

---
+++ src/com/github/catvl/base/Example2.java
@@ -0,0 +1,19 @@
+package com.github.catvl.base;
+
+import java.util.Arrays;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+
+        var1=1024;
+
+        System.out.println("Переменной var1 присвоено: " + var1);
+
+        var2= var1 / 2;
+
+        System.out.print("Переменной var2 содержит var1 / 2: ");
+        System.out.println(var2);
+    }
+}
