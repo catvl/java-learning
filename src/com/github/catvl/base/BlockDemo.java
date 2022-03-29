1f5344386d134c6a67de0fc38ecbdf98aef8e186
catvl <katrin030202@mail.ru>
2022-03-29 23:38:26 +0600

    used block

---
+++ src/com/github/catvl/base/BlockDemo.java
@@ -0,0 +1,17 @@
+package com.github.catvl.base;
+
+public class BlockDemo {
+    public static void main(String[] args) {
+        double i,j,d;
+
+        i=5;
+        j=10;
+
+        if(i!=0){
+            System.out.println("i не равно нулю");
+            d=j/i;
+            System.out.print("j/i равно: " + d);
+        }
+
+    }
+}
